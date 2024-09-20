129dc279a07fc71094cd9623cc2dbd8876d0fa63
LucianC2025 <lucian.cruz2025@gbwl.org>
2024-09-20 14:37:42 +0000

    edited main.py

--- main.py
+++ main.py
@@ -1,6 +1,73 @@
+# You can initialize important data at the tope of the
+score = 0
+trivia = [
+    # Q1
+      { 
+       'question': "What is Bowie's Favorite food? ",
+       'answer': 'pizza crust',
+       'options': ['chicken', 'kible', 'rabbit']
+      },
+    # Q2
+      { 
+       'question': "What adjective best describes Bowie?",
+       'answer': 'devious',
+       'options': ['ugly', 'mellow', 'cute']
+      },
+    # Q3
+      { 
+       'question': "What is Bowie's Favorite toy?",
+       'answer': 'white ball',
+       'options': ['bunny ball', 'pig ball', 'tire']
+      },
+     # Q4
+      { 
+       'question': 'How old is Bowie',
+       'answer': '3 years old',
+       'options': ['2 months old', '4 years old', '5 years old']
+      },
+    # Q5
+      { 
+       'question': '🥰Does Bowie love Lucian?🥰',
+       'answer': 'he better',
+       'options': ['not enough', 'yes', 'no']
+      },
+    # Q6
+      { 
+       'question': 'Where does Bowie like to be pet?',
+       'answer': 'the side of his face',
+       'options': ['his tummy', 'his legs', 'his back']
+      },
+    # Q7
+      { 
+       'question': "🤬Who is Bowie's opp?🤬",
+       'answer': 'bear🐕',
+       'options': ['the door man🧍‍♂️', 'lordy🐕', 'yellow dog🐕']
+      },
+    # Q8
+      { 
+       'question': 'How does Bowie feel about swimming?',
+       'answer': 'It is the scariest thing in the world and any body of water is the gates of hell',
+       'options': ['Its his favorite thing in the world', 'He tolerates it, but needs to be given spa treatment after', 'He rather not swim']
+      },
+    # Q9
+      { 
+       'question': "🥰Who is Bowie's favorite🥰",
+       'answer': "probably Lucian's dad",
+       'options': ["probably Lucian's sister","probably Lucian's mom", "probably lucian"]
+      }  
+    # Q10
+      { 
+       'question': "What is Bowie's Flaw?",
+       'answer': "He thinks he is alpha",
+       'options': ["he farts a lot","he's stubborn", "he insits on peeing in the home"]
+      }  
+]
+
+# Main Method for the game loop
 def main():
-    print("hello world")
+    print("Let's Play 🐩🐶 Bowie 🐶🐩 Trivia!")
 
 
 if __name__ == "__main__":
     main()
+   
