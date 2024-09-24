8866696349e19cf1bcf110f128cc3bde549cda98
LucianC2025 <lucian.cruz2025@gbwl.org>
2024-09-24 18:47:55 +0000

    edited main.py

--- main.py
+++ main.py
@@ -6,80 +6,81 @@ trivia = [
       { 
        'question': "What is Bowie's Favorite food?",
        'answer': 'pizza crust',
-       'options': ['chicken', 'kible', 'rabbit']
+       'options': ['chicken', 'kible', 'pizza crust','rabbit']
       },
     # Q2
       { 
        'question': "What adjective best describes Bowie?",
        'answer': 'devious',
-       'options': ['ugly', 'mellow', 'cute']
+       'options': ['ugly', 'devious', 'mellow', 'cute']
       },
     # Q3
       { 
        'question': "What is Bowie's Favorite toy?",
        'answer': 'white ball',
-       'options': ['bunny ball', 'pig ball', 'tire']
+       'options': ['white ball', 'bunny ball', 'pig ball', 'tire']
       },
      # Q4
       { 
        'question': '👶How old is Bowie👴',
        'answer': '3 years old',
-       'options': ['2 months old', '4 years old', '5 years old']
+       'options': ['2 months old', '4 years old', '3 years old','5 years old']
       },
     # Q5
       { 
        'question': '🥰Does Bowie love Lucian?🥰',
        'answer': 'he better',
-       'options': ['not enough', 'yes', 'no']
+       'options': ['not enough', 'he better', 'yes', 'no']
       },
     # Q6
       { 
        'question': '👨‍🍼💆‍♂️Where does Bowie like to be pet?💆‍♂️👨‍🍼',
        'answer': 'the side of his face',
-       'options': ['his tummy', 'his legs', 'his back']
+       'options': ['his tummy',' the side of his face', 'his legs', 'his back']
       },
     # Q7
       { 
        'question': "🤬Who is Bowie's opp?🤬",
        'answer': 'bear🐕',
-       'options': ['the door man🧍‍♂️', 'lordy🐕', 'yellow dog🐕']
+       'options': ['bear🐕', 'the door man🧍‍♂️', 'lordy🐕', 'yellow dog🐕']
       },
     # Q8
       { 
        'question': '🏊🏼‍♂️How does Bowie feel about swimming?🏊🏼‍♂️',
        'answer': 'It is the scariest thing in the world and any body of water is the gates of hell',
-       'options': ['Its his favorite thing in the world', 'He tolerates it, but needs to be given spa treatment after', 'He rather not swim']
+       'options': ['Its his favorite thing in the world', 'He tolerates it, but needs to be given spa treatment after', 'He rather not swim', 'It is the scariest thing in the world and any body of water is the gates of hell']
       },
     # Q9
       { 
        'question': "🥰Who is Bowie's favorite🥰",
        'answer': "probably Lucian's dad",
-       'options': ["probably Lucian's sister","probably Lucian's mom", "probably lucian"]
+       'options': ["probably Lucian's sister","probably Lucian's mom", "probably lucian", "probably Lucian's dad"]
       },
     # Q10
       { 
        'question': "What is Bowie's flaw?",
        'answer': "He thinks he is alpha",
-       'options': ["he farts a lot", "he's stubborn", "he insits on peeing in the home"]
+       'options': ["he farts a lot", "he's stubborn", "He thinks he is alpha", "he insits on peeing in the home"]
       }  
 ]
 
 # Dispaly the question and possible options
 # Function takes question (String), answer (String), options (list)
 # function returns a boolean
+
 def ask_question(question, answer, options):
   # 1. Print out a question from the Trivia list
-  for current in trivia:
+  print(question)
   
   # 2. Print out the options (list)
+  for option in options:
+    print(f"🐶{option}")
   
   # 3. get user input
-  # choice = input("Your answer:")
-  # print(choice)
+  choice = input("Enter answer: ")
   
   # 4. Check if choice matches correct answer
-  
-  return False
+
   
   
 # Main Method for the game loop
@@ -89,7 +90,6 @@ def main():
     score = 0
     
     # Starting with just ONE trivia item, loop laster
-    # Call ask_question funciton on each item in trivia list
     current = (trivia[0]) # prints the first dictionary
     # Get data from that item
     q = current['question'] # Prints the question (value of the first key)
